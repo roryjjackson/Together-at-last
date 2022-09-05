7f72b927d252e1ce544dac697c2ab795df028a27
rory jackson <roryjamesjackson@hotmail.com>
2022-09-05 16:33:18 +0100

    added has many weddings to user

---
+++ app/models/wedding.rb
@@ -0,0 +1,3 @@
+class Wedding < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_095810) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_153133) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,4 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_095810) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "weddings", force: :cascade do |t|
+    t.text "info"
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.string "page_heading"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_weddings_on_user_id"
+  end
+
+  add_foreign_key "weddings", "users"
 end
