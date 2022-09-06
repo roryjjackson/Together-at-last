1bb649852d0f85e79109bcab02606f451a5e46b3
Yanming Tse <tseyan113@gmail.com>
2022-09-06 15:54:42 +0100

    build the links between wedding and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "weddings#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :pages
+  resources :pages, :weddings
 
   # Defines the root path route ("/")
   # root "articles#index"
