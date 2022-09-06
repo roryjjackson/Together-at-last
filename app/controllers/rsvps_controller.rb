4819e9709bf98fd1fca06b128f1f7ee27e8cf9da
rory jackson <roryjamesjackson@hotmail.com>
2022-09-06 13:40:31 +0100

    created basic funtcion for rsvp form however it isnt saving

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,2 +1,42 @@
 class RsvpsController < ApplicationController
+  def index
+    @rsvps = Rsvp.all
+    # raise
+  end
+
+  def show
+    @rsvp = Rsvp.find(params[:id])
+  end
+
+  def new
+    @rsvp = Rsvp.new
+  end
+
+  def create
+    @rsvp = Rsvp.new(rsvp_params)
+    # raise
+    @rsvp.wedding_guests_id = current_user.id
+    @rsvp.save
+    # if @rsvp.save
+      redirect_to root_path(@rsvp)
+      raise
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+  end
+
+  # def edit
+  # end
+
+  # def destroy
+  # end
+
+  # def update
+  # end
+
+  private
+
+  def rsvp_params
+    params.require(:rsvp).permit(:meal_choice, :spotify_song, :attendance, :attendee)
+  end
 end
