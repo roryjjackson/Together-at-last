0ec13e910be7b3e58d4923cd9f9eb9baaae98524
rory jackson <roryjamesjackson@hotmail.com>
2022-09-06 10:55:08 +0100

    added controllers

---
+++ app/controllers/accomodation_info_controller.rb
@@ -0,0 +1,2 @@
+class AccomodationInfoController < ApplicationController
+end
---
+++ test/controllers/vips_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class VipsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
