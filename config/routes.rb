82b329e5a48ca5d062ba2e07ac4e92a73f510160
Yanming Tse <tseyan113@gmail.com>
2022-09-06 17:56:08 +0100

    adding home description page after sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "weddings#index"
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :weddings do
     resources :pages, only: [ :new, :create ]
