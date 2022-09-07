9c1692627e63cb5962d5cd9159bdc0397916b5e1
Yanming Tse <tseyan113@gmail.com>
2022-09-07 14:00:05 +0100

    fixed sign in and log in page path and make the form in a container

--- app/controllers/registries_controller.rb
+++ app/controllers/registries_controller.rb
@@ -1,2 +1,17 @@
 class RegistriesController < ApplicationController
+  def new
+    @registry = Registry.new
+  end
+
+  def create
+    @registry = Registry.new(@registry_id)
+    # raise
+    @registry.save
+    if @registry.save
+      redirect_to root_path(@registry)
+      raise
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
